3f5d1d6e85efd0b82dd21be223e24e2093e5fed7
OPSXCQ <opsxcq@strm.sh>
2017-04-01 23:45:42 -0300

    I'm not bad, I'm just drawn that way

--- src/server.py
+++ src/server.py
@@ -1,33 +1,47 @@
 import os
 import json
+from schedule import *
 import schedule
 import time
 import thread
+import subprocess
 from flask import Flask, request, abort
 from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
 # Webhook, Scheduled
-app.config["MODE"] = os.environ["MODE"]
+app.config["MODE"] = os.environ.get("MODE", None)
 
-# For webhook security
-app.config["TOKEN"] = os.environ["TOKEN"]
+if app.config["MODE"] == "cron":
+    print("[+] Starting in cron mode")
+    # If no interval is passed, it will be executed hourly
+    app.config["INTERVAL"] = os.environ.get("INTERVAL", 3600)
 
-app.config["INTERVAL"] = os.environ["INTERVAL"]
+if app.config["MODE"] == "http":
+    # For webhook security
+    app.config["TOKEN"] = os.environ.get("TOKEN", None)
 
-# Mail configurations
-app.config["MAIL_TITLE"] = os.environ["MAIL_TITLE"]
-app.config["MAIL_SERVER"] = os.environ["MAIL_SERVER"]
-app.config["MAIL_PORT"] = int(os.environ["MAIL_PORT"])
-app.config["MAIL_USE_SSL"] = bool(os.environ["MAIL_USE_SSL"])
-app.config["MAIL_USERNAME"] = os.environ["MAIL_USERNAME"]
-app.config["MAIL_PASSWORD"] = os.environ["MAIL_PASSWORD"]
+# Notifications
+app.config["NOTIFY"] = os.environ.get("NOTIFY", None)
 
-mail = Mail(app)
+if app.config["NOTIFY"] == "email":
+    # Mail configurations
+    app.config["MAIL_TITLE"] = os.environ["MAIL_TITLE"]
+    app.config["MAIL_SERVER"] = os.environ["MAIL_SERVER"]
+    app.config["MAIL_PORT"] = int(os.environ["MAIL_PORT"])
+    app.config["MAIL_USE_SSL"] = bool(os.environ["MAIL_USE_SSL"])
+    app.config["MAIL_USERNAME"] = os.environ["MAIL_USERNAME"]
+    app.config["MAIL_PASSWORD"] = os.environ["MAIL_PASSWORD"]
 
-sender = os.environ["MAIL_SENDER"]
-recipient = os.environ["MAIL_RECIPIENT"]
+    mail = Mail(app)
+
+    sender = os.environ["MAIL_SENDER"]
+    recipient = os.environ["MAIL_RECIPIENT"]
+
+if not os.path.isfile("/task.sh"):
+    print("[+] /task.sh or /task.py not found, please specify a task")
+    exit(-1)
 
 status='WAITING'
 
@@ -39,14 +53,15 @@ def webhookTrigger():
 def getStatus():
     return status
 
-def schedule():
-    schedule.every().wednesday.at("00:15").do(task)
+def scheduler():
+    schedule.every(10).seconds.do(task)
     while True:
         schedule.run_pending()
         time.sleep(1)
 
 def task():
-    print("Doing the task")
+    print("[+] Executing the task")
+    result = subprocess.run(['ls', '-l'], stdout=subprocess.PIPE)
 
 def sendEmail(title, body):
     msg = Message(title,
@@ -60,5 +75,5 @@ def index():
     return "This is a service, and you are using it wrong !"
 
 if __name__ == "__main__":
-    thread.start_new_thread (schedule)
+    thread.start_new_thread (scheduler,())
     app.run(host='0.0.0.0', port=8080,threaded=True)
